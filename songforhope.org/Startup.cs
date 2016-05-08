afbd4cf832a15a70dbfaef4133b25dabacf65267
John Velis <john@johnvelis.com>
2016-05-08 07:46:42 -0400

    initial commit

---
+++ songforhope.org/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(songforhope.org.Startup))]
+namespace songforhope.org
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
